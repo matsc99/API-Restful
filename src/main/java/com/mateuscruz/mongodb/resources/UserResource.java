e53e026eda62d80904089411dbff9c7554803412
matsc99 <mateus.sousa.cruz@hotmail.com>
2024-02-19 15:51:13 -0300

    Obtendo usuario pelo id

--- src/main/java/com/mateuscruz/mongodb/resources/UserResource.java
+++ src/main/java/com/mateuscruz/mongodb/resources/UserResource.java
@@ -6,11 +6,13 @@ import com.mateuscruz.mongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,5 +27,11 @@ public class UserResource {
         List<User> list = service.findAll();
         List<UserDTO> listDto= list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<List<UserDTO>> findById(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(Collections.singletonList(new UserDTO(obj)));
     }
 }
