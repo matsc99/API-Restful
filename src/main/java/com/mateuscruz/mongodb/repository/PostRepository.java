b8525473924a063a76e0d09c4ed3505ee58c5d2f
matsc99 <mateus.sousa.cruz@hotmail.com>
2024-02-20 16:56:26 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/mateuscruz/mongodb/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.mateuscruz.mongodb.repository;
+
+import com.mateuscruz.mongodb.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
