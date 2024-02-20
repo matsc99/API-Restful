bc4a8e057fae561044e2d4aa1b125d7cf647d226
matsc99 <mateus.sousa.cruz@hotmail.com>
2024-02-20 17:10:18 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/mateuscruz/mongodb/config/Instantiation.java
+++ src/main/java/com/mateuscruz/mongodb/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.mateuscruz.mongodb.config;
 
 import com.mateuscruz.mongodb.domain.Post;
 import com.mateuscruz.mongodb.domain.User;
+import com.mateuscruz.mongodb.dto.AuthorDTO;
 import com.mateuscruz.mongodb.repository.PostRepository;
 import com.mateuscruz.mongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,10 +36,13 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+
     }
 }
