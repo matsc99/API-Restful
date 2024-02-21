e6d483b759bb5de46de41c0ab74dfd36c98358d0
matsc99 <mateus.sousa.cruz@hotmail.com>
2024-02-21 13:55:54 -0300

    Obtendo um post por id

---
+++ src/main/java/com/mateuscruz/mongodb/resources/exception/PostResource.java
@@ -0,0 +1,21 @@
+package com.mateuscruz.mongodb.resources.exception;
+
+import com.mateuscruz.mongodb.domain.Post;
+import com.mateuscruz.mongodb.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ src/main/java/com/mateuscruz/mongodb/services/PostService.java
@@ -0,0 +1,20 @@
+package com.mateuscruz.mongodb.services;
+
+import com.mateuscruz.mongodb.domain.Post;
+import com.mateuscruz.mongodb.repository.PostRepository;
+import com.mateuscruz.mongodb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
